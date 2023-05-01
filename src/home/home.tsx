61cce8e200647462e45178f01ef48fa76159e9c8
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-05-01 16:53:29 -0300

    chore: homepage content

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,10 +1,120 @@
+import { Footer } from "../components/footer/footer";
 import { Header } from "../components/header/header";
-import { HomeContainer } from "./style_home";
+import {
+  HomeSection,
+  HomeContainer,
+  HomeH3,
+  HomeP,
+  HomeUl,
+  HomeLi,
+  HomeA,
+} from "./style_home";
 
 export function Home() {
   return (
     <HomeContainer>
       <Header />
+
+      <HomeSection>
+        <HomeH3></HomeH3>
+        <HomeP>
+          Sou Gregório,
+          <br />
+          Desenvolvedor Fullstack com principais habilidades em React,
+          Typescript, NodeJS e C# (Entity Framework), hoje estou atuando como
+          Freelancer em Cachoeira dos Índios - Paraíba
+        </HomeP>
+      </HomeSection>
+
+      <HomeSection>
+        <HomeH3>Habilidades e conhecimentos:</HomeH3>
+        <HomeUl>
+          <HomeLi>• React</HomeLi>
+          <HomeLi>• Typescript</HomeLi>
+          <HomeLi>• NodeJS</HomeLi>
+          <HomeLi>• C# (Entity Framework)</HomeLi>
+          <HomeLi>• ExpressJS</HomeLi>
+          <HomeLi>• NestJS</HomeLi>
+          <HomeLi>• PostgreSQL</HomeLi>
+          <HomeLi>• SQL Server</HomeLi>
+          <HomeLi>• MySQL</HomeLi>
+          <HomeLi>• MongoDB</HomeLi>
+          <HomeLi>• Java (Estudando no programa Start Capgemini)</HomeLi>
+          <HomeLi>• Angular (Estudando no programa Start Capgemini)</HomeLi>
+        </HomeUl>
+      </HomeSection>
+
+      <HomeSection>
+        <HomeH3>Certificados:</HomeH3>
+        <HomeUl>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Fullstack Developer Mod 6 Bootcamp II - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Fullstack Developer Mod 6 Bootcamp I - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Fullstack Developer Mod 5 - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Fullstack Developer Mod 4 - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Fullstack Developer Mod 3 - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Web Banco de Dados/Cloud Mod 2 - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • Lógica de Programação Mod 1 - BlueEdTech
+            </HomeA>
+          </HomeLi>
+          <HomeLi>
+            <HomeA
+              href="https://www.udemy.com/certificate/UC-d2598b6c-ab55-4105-b685-fe2eaa472037/"
+              target="blank"
+            >
+              • .Net 6 Web Api - Profº Stephany Henrique de Almeida Batista
+            </HomeA>
+          </HomeLi>
+        </HomeUl>
+      </HomeSection>
+
+      <Footer />
     </HomeContainer>
   );
 }
--- src/home/style_home.ts
+++ src/home/style_home.ts
@@ -1,6 +1,46 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div`
+export const HomeContainer = styled.main`
   width: 100%;
   height: 100vh;
+`;
+
+export const HomeSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 40px 200px 10px 200px;
+
+  @media (max-width: 700px) {
+    padding: 40px 30px 10px 30px;
+  }
+`;
+
+export const HomeH3 = styled.h3`
+  color: white;
+`;
+
+export const HomeP = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: start;
+  color: white;
+`;
+
+export const HomeUl = styled.ul`
+  list-style: none;
+`;
+
+export const HomeLi = styled.li`
+  color: white;
+  padding-left: 10px;
+`;
+
+export const HomeA = styled.a`
+  text-decoration: none;
+  color: white;
+
+  &:hover {
+    color: rgb(0, 175, 239);
+  }
 `;
